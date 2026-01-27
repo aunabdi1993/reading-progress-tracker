a72c8b38836ba753a04b35ad8552611b55c83dfa
Aun Abdi <aabdidevelopment@gmail.com>
2026-01-27 23:42:50 +0000

    Add enum model

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from typing import Union
+from enum import Enum
 
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -9,6 +10,11 @@ class Item(BaseModel):
     name: str
     price: float
     is_offer: Union[bool, None] = None
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 @app.get("/")
 def read_root():
@@ -21,3 +27,13 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
